4dc2375a3557e8752f852c81c45593a2b2a8378c
femeunier <felicien.meunier@gmail.com>
2019-10-31 10:01:45 -0400

    Add script

--- scripts/create_census_fileED.R
+++ scripts/create_census_fileED.R
@@ -22,7 +22,20 @@ data.wytham <- data.wytham %>% rename(x =  stemlocx_.m.,
                                                                               dbh_census = 100*dbh_census) %>%
   dplyr::select(c(x,y,dbh_tls,h,CA,AGV_m,dbh_census))
 
+extr_x <- extremum(data.wytham[["x"]])
+extr_y <- extremum(data.wytham[["y"]])
 patch_X <- 20
 patch_Y <- 20
 
 data.wytham[["patch"]] <- patchnumber_from_position(data.wytham[["x"]],data.wytham[["y"]],patch_X,patch_Y)
+
+ggplot(data.wytham,
+       aes(x = x-extr_x[1], y = y-extr_y[1], color = as.factor(patch),size = dbh_census)) +
+  geom_point(alpha = 0.5) +
+  xlab("x (m)") +
+  ylab("y (m)") +
+  scale_x_continuous(breaks = seq(extr_x[1], extr_x[2], patch_X)-extr_x[1]) +
+  scale_y_continuous(breaks = seq(extr_y[1], extr_y[2], patch_Y)-extr_y[1]) +
+  theme_bw() +
+  theme(legend.position = "none")
+
