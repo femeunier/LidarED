697f92e62e1bb37563a53b1b724a5212d97be8e8
femeunier <felicien.meunier@gmail.com>
2020-03-19 14:10:55 -0500

    bugfix to tfiles

--- scripts/restart.to.write.tfiles.R
+++ scripts/restart.to.write.tfiles.R
@@ -2,6 +2,7 @@ rm(list = ls())
 
 library(LidarED)
 library(PEcAn.ED2)
+library(ED2scenarios)
 
 
 main.dir <- "/data/gent/vo/000/gvo00074/pecan/output/other_runs/Wytham/growth_storage_resp/run"
@@ -14,18 +15,28 @@ for (isimu in seq(1,length(simulations))){
   ed2in.file <- "/home/carya/ED2/ED/run/ED2IN"
   ed2in.file.dest <- "/home/carya/ED2/ED/run/ED2IN_mod"
   ed2in <- read_ed2in(ed2in.file)
-  
+
   # History run
   ed2in$RUNTYPE <- "HISTORY"
-  
+
   # Activate T-files
-  
+
   ed2in$ITOUTPUT <- 3
   ed2in$SFILIN <- ed2in$SFILOUT
-  
+
   # IC
   ed2in$IDATEH <- 1
   ed2in$IMONTHH <- 1
   ed2in$IYEARH <- year.init[isimu]
-  
+
+  write_job_noR(file =  file.path(run_scenar,"job.sh"),
+                nodes = 1,ppn = 18,mem = 16,walltime = 24,
+                prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
+                CD = run_scenar,
+                ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
+                ED2IN = "ED2IN")
+
+  setwd(file.path(main.dir,"run"))
+
+  system2("bash","job.sh")
 }
