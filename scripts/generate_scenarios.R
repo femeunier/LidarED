bfc236037a3e5b12b1950db297e184f4dcbb8510
FULLNAME <you@yourdomain.example.com>
2020-03-19 19:57:03 +0100

    Add resp scenarios

--- scripts/generate_scenarios.R
+++ scripts/generate_scenarios.R
@@ -5,19 +5,27 @@ library(ED2scenarios)
 library(LidarED)
 library(PEcAn.ED2)
 
-ref_dir <- "/home/carya/R/ED2scenarios/reference"
+ref_dir <- "/scratch/gent/vo/000/gvo00074/hackaton/run_Wytham/ref"
 ed2in <- read_ed2in(file.path(ref_dir,"ED2IN"))
+ed2in$ITOUTPUT <- 3
 
-rundir <- "/home/carya/R/LidarED/runs/run"
-outdir <- "/home/carya/R/LidarED/runs/out"
+rundir <- "/data/gent/vo/000/gvo00074/pecan/output/other_runs/Wytham/growth_storage_resp/run"
+outdir <- "/data/gent/vo/000/gvo00074/pecan/output/other_runs/Wytham/growth_storage_resp/out"
 
 if(!dir.exists(rundir)) dir.create(rundir)
 if(!dir.exists(outdir)) dir.create(outdir)
 
 all_configs <- readRDS(file = './data/all_configs.RDS')
 all_scenarios <- c("Hmax","Hmean","CA","AGB","Bl")
+
 add2config <- list(SLA = 36.7*0.48,
-                   Vm0 = 32.85) # From TRY
+                   Vm0 = 32.85,
+                   growth_resp_factor = 0.45,
+                   storage_turnover_rate = 0.166) # From TRY
+
+add2config_min <- list(growth_resp_factor = 0.45,
+                   storage_turnover_rate = 0.166)
+
 
 x2 <- combn(1:5,2)
 x3 <- combn(1:5,3)
@@ -51,6 +59,37 @@ pft_name <- "temperate.North_Mid_Hardwood"
 list_dir <- list()
 
 #############################################################################################
+# Reference no config
+
+run_ref <- file.path(rundir,"reference_noconfig")
+out_ref <- file.path(outdir,"reference_noconfig")
+
+if(!dir.exists(run_ref)) dir.create(run_ref)
+if(!dir.exists(out_ref)) dir.create(out_ref)
+if(!dir.exists(file.path(out_ref,"analy"))) dir.create(file.path(out_ref,"analy"))
+if(!dir.exists(file.path(out_ref,"histo"))) dir.create(file.path(out_ref,"histo"))
+
+
+# ED2IN
+ed2in_scenar <- ed2in
+ed2in_scenar$IEDCNFGF <- file.path(run_ref,"")
+ed2in_scenar$FFILOUT = file.path(out_ref,"analy","analysis")
+ed2in_scenar$SFILOUT = file.path(out_ref,"histo","history")
+
+write_ed2in(ed2in_scenar,filename = file.path(run_ref,"ED2IN"))
+
+# job.sh
+write_job(file =  file.path(run_ref,"job.sh"),
+          nodes = 1,ppn = 18,mem = 16,walltime = 24,
+          prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
+          CD = run_ref,
+          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
+          ED2IN = "ED2IN")
+
+list_dir[["reference_noconfig"]] = run_ref
+
+
+#############################################################################################
 # Reference
 
 run_ref <- file.path(rundir,"reference")
@@ -58,10 +97,13 @@ out_ref <- file.path(outdir,"reference")
 
 if(!dir.exists(run_ref)) dir.create(run_ref)
 if(!dir.exists(out_ref)) dir.create(out_ref)
+if(!dir.exists(file.path(out_ref,"analy"))) dir.create(file.path(out_ref,"analy"))
+if(!dir.exists(file.path(out_ref,"histo"))) dir.create(file.path(out_ref,"histo"))
+
 
 # ED2IN
 ed2in_scenar <- ed2in
-ed2in_scenar$IEDCNFGF <- file.path(run_ref,"")
+ed2in_scenar$IEDCNFGF <- file.path(run_ref,"config.xml")
 ed2in_scenar$FFILOUT = file.path(out_ref,"analy","analysis")
 ed2in_scenar$SFILOUT = file.path(out_ref,"histo","history")
 
@@ -81,10 +123,53 @@ write_job(file =  file.path(run_ref,"job.sh"),
           nodes = 1,ppn = 18,mem = 16,walltime = 24,
           prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
           CD = run_ref,
-          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run/ed_2.1-opt",
+          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
           ED2IN = "ED2IN")
 
 list_dir[["reference"]] = run_ref
+
+
+
+#############################################################################################
+# Reference respiration
+
+run_ref <- file.path(rundir,"reference_resp")
+out_ref <- file.path(outdir,"reference_resp")
+
+if(!dir.exists(run_ref)) dir.create(run_ref)
+if(!dir.exists(out_ref)) dir.create(out_ref)
+if(!dir.exists(file.path(out_ref,"analy"))) dir.create(file.path(out_ref,"analy"))
+if(!dir.exists(file.path(out_ref,"histo"))) dir.create(file.path(out_ref,"histo"))
+
+
+# ED2IN
+ed2in_scenar <- ed2in
+ed2in_scenar$IEDCNFGF <- file.path(run_ref,"config.xml")
+ed2in_scenar$FFILOUT = file.path(out_ref,"analy","analysis")
+ed2in_scenar$SFILOUT = file.path(out_ref,"histo","history")
+
+write_ed2in(ed2in_scenar,filename = file.path(run_ref,"ED2IN"))
+
+# Config
+config_resp <- list()
+config_resp[[pft_name]] <- unlist(add2config_min)
+xml <- write.config.xml.ED2(defaults = defaults,
+                            settings = settings, trait.values = config_resp)
+
+XML::saveXML(xml, file = file.path(run_ref,"config.xml"), indent = TRUE,
+             prefix = PREFIX_XML)
+
+# job.sh
+write_job(file =  file.path(run_ref,"job.sh"),
+          nodes = 1,ppn = 18,mem = 16,walltime = 24,
+          prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
+          CD = run_ref,
+          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
+          ED2IN = "ED2IN")
+
+list_dir[["reference_resp"]] = run_ref
+
+
 
 ###############################################################################################
 # Near-bare ground
@@ -94,9 +179,98 @@ out_ref2 <- file.path(outdir,"near_bare_ground")
 
 if(!dir.exists(run_ref2)) dir.create(run_ref2)
 if(!dir.exists(out_ref2)) dir.create(out_ref2)
+if(!dir.exists(file.path(out_ref2,"analy"))) dir.create(file.path(out_ref2,"analy"))
+if(!dir.exists(file.path(out_ref2,"histo"))) dir.create(file.path(out_ref2,"histo"))
+
 
 # ED2IN
 ed2in_scenar <- ed2in
+ed2in_scenar$IYEARA <- 1712
+ed2in_scenar$IED_INIT_MODE <- 0
+ed2in_scenar$IEDCNFGF <- file.path(run_ref2,"config.xml")
+ed2in_scenar$FFILOUT = file.path(out_ref2,"analy","analysis")
+ed2in_scenar$SFILOUT = file.path(out_ref2,"histo","history")
+
+write_ed2in(ed2in_scenar,filename = file.path(run_ref2,"ED2IN"))
+
+# Config
+
+config_TRY <- list()
+config_TRY[[pft_name]] <- unlist(add2config)
+xml <- write.config.xml.ED2(defaults = defaults,
+                            settings = settings, trait.values = config_TRY)
+
+
+XML::saveXML(xml, file = file.path(run_ref2,"config.xml"), indent = TRUE,
+             prefix = PREFIX_XML)
+
+# job.sh
+write_job(file =  file.path(run_ref2,"job.sh"),
+          nodes = 1,ppn = 18,mem = 16,walltime = 48,
+          prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
+          CD = run_ref2,
+          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
+          ED2IN = "ED2IN")
+
+list_dir[["near_bare_ground"]] = run_ref2
+
+
+###############################################################################################
+# Near-bare ground resp
+
+run_ref2 <- file.path(rundir,"near_bare_ground_resp")
+out_ref2 <- file.path(outdir,"near_bare_ground_resp")
+
+if(!dir.exists(run_ref2)) dir.create(run_ref2)
+if(!dir.exists(out_ref2)) dir.create(out_ref2)
+if(!dir.exists(file.path(out_ref2,"analy"))) dir.create(file.path(out_ref2,"analy"))
+if(!dir.exists(file.path(out_ref2,"histo"))) dir.create(file.path(out_ref2,"histo"))
+
+
+# ED2IN
+ed2in_scenar <- ed2in
+ed2in_scenar$IYEARA <- 1712
+ed2in_scenar$IED_INIT_MODE <- 0
+ed2in_scenar$IEDCNFGF <- file.path(run_ref2,"config.xml")
+ed2in_scenar$FFILOUT = file.path(out_ref2,"analy","analysis")
+ed2in_scenar$SFILOUT = file.path(out_ref2,"histo","history")
+
+write_ed2in(ed2in_scenar,filename = file.path(run_ref2,"ED2IN"))
+
+# Config
+config_resp <- list()
+config_resp[[pft_name]] <- unlist(add2config_min)
+xml <- write.config.xml.ED2(defaults = defaults,
+                            settings = settings, trait.values = config_resp)
+
+XML::saveXML(xml, file = file.path(run_ref2,"config.xml"), indent = TRUE,
+             prefix = PREFIX_XML)
+
+# job.sh
+write_job(file =  file.path(run_ref2,"job.sh"),
+          nodes = 1,ppn = 18,mem = 16,walltime = 48,
+          prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
+          CD = run_ref2,
+          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
+          ED2IN = "ED2IN")
+
+list_dir[["near_bare_ground_resp"]] = run_ref2
+
+###############################################################################################
+# Near-bare ground no config
+
+run_ref2 <- file.path(rundir,"near_bare_ground_noconfig")
+out_ref2 <- file.path(outdir,"near_bare_ground_noconfig")
+
+if(!dir.exists(run_ref2)) dir.create(run_ref2)
+if(!dir.exists(out_ref2)) dir.create(out_ref2)
+if(!dir.exists(file.path(out_ref2,"analy"))) dir.create(file.path(out_ref2,"analy"))
+if(!dir.exists(file.path(out_ref2,"histo"))) dir.create(file.path(out_ref2,"histo"))
+
+
+# ED2IN
+ed2in_scenar <- ed2in
+ed2in_scenar$IYEARA <- 1712
 ed2in_scenar$IED_INIT_MODE <- 0
 ed2in_scenar$IEDCNFGF <- file.path(run_ref2,"")
 ed2in_scenar$FFILOUT = file.path(out_ref2,"analy","analysis")
@@ -104,19 +278,18 @@ ed2in_scenar$SFILOUT = file.path(out_ref2,"histo","history")
 
 write_ed2in(ed2in_scenar,filename = file.path(run_ref2,"ED2IN"))
 
-# Config
-XML::saveXML(xml, file = file.path(run_ref2,"config.xml"), indent = TRUE,
-             prefix = PREFIX_XML)
 
 # job.sh
 write_job(file =  file.path(run_ref2,"job.sh"),
-          nodes = 1,ppn = 18,mem = 16,walltime = 24,
+          nodes = 1,ppn = 18,mem = 16,walltime = 48,
           prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
           CD = run_ref2,
-          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run/ed_2.1-opt",
+          ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
           ED2IN = "ED2IN")
 
-list_dir[["near_bare_ground"]] = run_ref2
+list_dir[["near_bare_ground_noconfig"]] = run_ref2
+
+
 
 ####################################################################################################################
 # All combinations
@@ -129,6 +302,8 @@ for (i in seq_len(length(cmbs))){
 
   if(!dir.exists(run_scenar)) dir.create(run_scenar)
   if(!dir.exists(out_scenar)) dir.create(out_scenar)
+  if(!dir.exists(file.path(out_scenar,"analy"))) dir.create(file.path(out_scenar,"analy"))
+  if(!dir.exists(file.path(out_scenar,"histo"))) dir.create(file.path(out_scenar,"histo"))
 
   # config file
   trait_values <- list()
@@ -148,6 +323,10 @@ for (i in seq_len(length(cmbs))){
   ed2in_scenar$FFILOUT = file.path(out_scenar,"analy","analysis")
   ed2in_scenar$SFILOUT = file.path(out_scenar,"histo","history")
 
+  if (grepl("CA",names_scenar)){
+     ed2in_scenar$CROWN_MOD <- 1
+  }
+
   write_ed2in(ed2in_scenar,filename = file.path(run_scenar,"ED2IN"))
 
   # job.sh
@@ -155,7 +334,7 @@ for (i in seq_len(length(cmbs))){
             nodes = 1,ppn = 18,mem = 16,walltime = 24,
             prerun = "ml UDUNITS/2.2.26-intel-2018a R/3.4.4-intel-2018a-X11-20180131 HDF5/1.10.1-intel-2018a; ulimit -s unlimited",
             CD = run_scenar,
-            ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/run/ed_2.1-opt",
+            ed_exec = "/user/scratchkyukon/gent/gvo000/gvo00074/felicien/ED2/ED/build/ed_2.1-opt",
             ED2IN = "ED2IN")
 
   list_dir[[names_scenar]]=run_scenar
